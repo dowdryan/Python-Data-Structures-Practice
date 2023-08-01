bc4a8e60f0d93a5ae147e175603f9aadb4e3ed43
Ryan <125604520+dowdryan@users.noreply.github.com>
2023-08-01 15:17:44 -0400

    Update two_oldest_ages.py

--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -16,7 +16,6 @@ def two_oldest_ages(ages):
     sorted_ages = sorted(ages, reverse=True)
     return sorted_ages[1], sorted_ages[0]
 
-# Test cases
-print(two_oldest_ages([1, 2, 10, 8]))  # Output: (8, 10)
-print(two_oldest_ages([6, 1, 9, 10, 4]))  # Output: (9, 10)
-print(two_oldest_ages([1, 5, 5, 2]))  # Output: (2, 5)
+print(two_oldest_ages([1, 2, 10, 8]))
+print(two_oldest_ages([6, 1, 9, 10, 4]))
+print(two_oldest_ages([1, 5, 5, 2]))
